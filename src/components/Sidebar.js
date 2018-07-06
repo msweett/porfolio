65142eb4ac500d4867132aff4cdf0db6c6034d75
Matthew Sweett <msweett1@gmail.com>
2018-07-06 12:43:46 -0300

    modified sidebar styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,19 @@
-import React from 'react'
+import React from "react";
 
 class Sidebar extends React.Component {
-    render() {
-        return (
-            <div class="sidebar">
-                <h4>{this.props.title}</h4>
-                <ul class="sidebar-list">
-                    <li >
-                        July 1st, 2018
-                    </li>
-                    <li >
-                        July 2nd, 2018
-                    </li>
-                    <li >
-                        July 3rd, 2018
-                    </li>
-                    <li>
-                        July 4th, 2018
-                    </li>
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div class="sidebar">
+        <h4>{this.props.title}</h4>
+        <ul class="sidebar-list">
+          <li>July 1st</li>
+          <li>July 2nd</li>
+          <li>July 3rd</li>
+          <li>July 4th</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Sidebar
+export default Sidebar;
